92557bfe5748daf1827be8bd664f091be634e92b
Daniil Slabov <dslabov@redrocket.software>
2024-06-28 17:47:33 +0300

    Update content for contact section

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -6,8 +6,8 @@ import EnvelopeIcon from '/public/icons/envelope.svg';
 
 const Contact = () => {
   return (
-    <SectionWrapper className='section-spacing' id="contacts">
-      <div className='grid grid-cols-1 md:grid-cols-2 overflow-hidden rounded-lg'>
+    <SectionWrapper className='section-spacing' id='contacts'>
+      <div className='grid grid-cols-1 overflow-hidden rounded-lg md:grid-cols-2'>
         <div className='bg-blue-600 p-4 md:p-6 lg:p-10'>
           <div className='flex flex-col gap-6 md:gap-10'>
             <h2 className='headline-small md:headline-med w-fit rounded-3xl border border-solid border-basic-white px-4 py-1 text-basic-white'>
@@ -15,26 +15,26 @@ const Contact = () => {
             </h2>
             <div>
               <p className=' b1m-body-med md:h6-headline-med lg:h5-headline text-blue-100'>
-                For any external relations please use emails below
+                For all matters please use emails below
               </p>
               <a
                 href='tel:+357 - 22030814'
-                className='mt-6 flex flex-row items-center gap-1 text-blue-100 b1m-body-reg'
+                className='b1m-body-reg mt-6 flex flex-row items-center gap-1 text-blue-100'
               >
                 <PhoneIcon className='fill-blue-100' /> +357 - 22030814
               </a>
-              <div className='flex md:flex-col lg:flex-row flex-wrap md:gap-1 lg:gap-4 mt-4'>
+              <div className='mt-4 flex flex-wrap md:flex-col md:gap-1 lg:flex-row lg:gap-4'>
                 <a
                   href='mailto:skukhno@movchans.com '
-                  className='flex flex-row items-center gap-1 text-blue-100  b1m-body-reg'
+                  className='b1m-body-reg flex flex-row items-center gap-1  text-blue-100'
                 >
                   <EnvelopeIcon className='fill-blue-100' />{' '}
                   skukhno@movchans.com
                 </a>
-                <div className='w-[1px] h-[27px] bg-gold-500 hidden lg:flex'></div>
+                <div className='hidden h-[27px] w-[1px] bg-gold-500 lg:flex'></div>
                 <a
                   href='mailto:contact@movchans.com'
-                  className='text-blue-100  b1m-body-reg ml-7 lg:ml-0'
+                  className='b1m-body-reg  ml-7 text-blue-100 lg:ml-0'
                 >
                   contact@movchans.com
                 </a>
@@ -45,8 +45,8 @@ const Contact = () => {
         <Image
           {...ContactBg}
           quality={100}
-          className='object-cover object-top h-[180px] md:h-full'
-          alt='For any external relations please use emails below'
+          className='h-[180px] object-cover object-top md:h-full'
+          alt='For all matters please use emails below'
         />
       </div>
     </SectionWrapper>
